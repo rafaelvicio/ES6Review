94621cbcf67736ad66acf55e2dd6650eab0e000d
Rafael Vicio <rafaelvicio@icloud.com>
2018-06-03 15:37:50 -0300

    const e let

---
+++ constLet/const.js
@@ -0,0 +1,21 @@
+const dataNascimento = '11/08/1996';
+
+dataNascimento = '11 de Agosto de 1996'; //Vai da erro
+console.log('Data de Nascimento', dataNascimento);
+
+const pessoa = {
+    nome: 'Rafael'
+};
+pessoa.idade = 21;
+
+console.log('Pessoa', pessoa)
+
+const pessoa = {
+    nome: 'Rafael'
+}
+
+const novaPessoa = {
+    nome: 'Rafael'
+}
+
+pessoa = novaPessoa // Vai da erro
---
+++ constLet/let.js
@@ -0,0 +1,8 @@
+let resultado = 0;
+soma = function(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+resultado = soma(2, 3);
+
+console.log('Resultado', resultado);

691e7331038b6a516e6ddf6beb22925613eda5a4
Rafael Vicio <rafaelvicio@icloud.com>
2018-06-03 15:51:07 -0300

    template string

---
+++ templateString/templateString.js
@@ -0,0 +1,12 @@
+const login = 'ecmascript';
+const dia = '13 de setembro';
+const ano = 2016;
+
+const mensagem = "Olá, " + login + "! \nHoje e: " + dia + " de" + ano;
+console.log(mensagem);
+
+const nome = 'Diego';
+console.log('Bem-vindo ', nome);
+
+const nome = 'Diego';
+console.log(`Bem-vindo, ${nome}`);
